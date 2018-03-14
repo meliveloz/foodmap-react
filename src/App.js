bf9727b7f153da7072b6e3edb1b90c7c43d912ac
tazocar <tankodani@gmail.com>
2018-03-14 19:54:16 -0300

    console.log food types - can't render

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import Search from './components/Search';
+import SearchButtons from './components/SearchButtons';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <Search></Search>
+      <div>
+        <Search></Search>
+        <SearchButtons></SearchButtons>
+      </div>
     );
   }
 }
---
+++ src/components/SearchButtons.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class SearchButtons extends Component {
+  state = {
+    inputSearch: ''
+  }
+  handleChange = (e) => {
+    this.setState({inputSearch: e.target.value});
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let city = "83";
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('user-key', '57800fe3a17bc04ca2e40cfb5cf036fc');
+    let options = {
+      method: 'GET',
+      headers: headers
+    };
+    let url = "https://developers.zomato.com/api/v2.1/cuisines?city_id=" + city;
+    window.fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+      data.cuisines.map(element => {
+        console.log(element.cuisine.cuisine_name)
+      })
+    })
+  }
+  render() {
+    return(
+      <form onSubmit = {this.handleSubmit}>
+        <div className="search-by-button">
+          <input onChange={this.handleChange} type="text" className=""  size="120"placeholder="¿Qué andas buscando?" />
+          <button className="btn">Buscar</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default SearchButtons
