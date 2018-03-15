08e15f4c87a81febc66ca89f71fdadf83959af30
meliveloz <mvelozcampos@gmail.com>
2018-03-15 16:50:55 -0300

    thumbnail cards

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Restaurants from './components/Restaurants.js';
 import './App.css';
 
 class App extends Component {
-
-
-
-    render() {
-
-            return(
-                <div>
-                <Search />
-                <Restaurants />
-                </div>
-            );
-        }
+  render() {
+    return(
+      <div>
+        <Search />
+        <br></br>
+          <div className="container">
+            <div className="row">
+              <Restaurants />
+            </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 
--- src/components/RestaurantsItems.js
+++ src/components/RestaurantsItems.js
@@ -5,7 +5,7 @@ class RestaurantsItems extends Component {
     let img = props.restaurants.featured_image;
     // si el key está vacío, elige una imagen random de unsplash
     if(img === '') {
-      return "https://source.unsplash.com/200x200/?cuisine"
+      return "https://source.unsplash.com/500x300/?cuisine"
     } else {
       // si no, muestra la imagen de la api
       return img
@@ -14,19 +14,19 @@ class RestaurantsItems extends Component {
   render() {
     //console.log(this.state);
     return (
-      <div className="col-lg-3">
-        <div className="card">  
+      <div className="col-xs-12 col-sm-4 col-lg-3 controlHeight">
+        <div className="card">               
+          <img className="img-responsive" src={this.checkImages(this.props)} />
           <div className="card-content">
-            <span className="card-title">{this.props.restaurants.name}</span>
-            <img src={this.checkImages(this.props)} alt="" />
-            <p>Average Cost for 2
-              = {this.props.restaurants.currency} {this.props.restaurants.average_cost_for_two}</p>
-            <br/>
-            <p>Average User rating = {this.props.restaurants.user_rating.aggregate_rating}</p>
-            <p>Address: {this.props.restaurants.location.address}</p>
+            <h4 className="card-title">
+             <em> {this.props.restaurants.name} </em>
+            </h4>
+            <p><strong>Promedio para dos: </strong>= {this.props.restaurants.currency} {this.props.restaurants.average_cost_for_two}</p>
+            <p><strong>Promedio rating: </strong> = {this.props.restaurants.user_rating.aggregate_rating}</p>
+            <p><strong>Dirección:</strong> {this.props.restaurants.location.address}</p>
           </div>
-          <div className="card-action">
-            <a href={this.props.restaurants.url}>Look us up on Zomato!</a>
+          <div className="card-read-more">
+            <a href={this.props.restaurants.url} className="btn btn-link btn-block"> Read More </a>
           </div>
         </div>
       </div>
@@ -36,3 +36,4 @@ class RestaurantsItems extends Component {
 
 
 export default RestaurantsItems;
+
