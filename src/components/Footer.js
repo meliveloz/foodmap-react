339500da96fa75028be257f4bfbacb3f5f7cc407
tazocar <tankodani@gmail.com>
2018-03-15 23:17:09 -0300

    stash

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="col-12 clear-margin">
+          <p className="footer-text">Hecho con <span className="heart">❤</span> por Foodies</p>
+        </div>
+      </footer>
+    );
+  }
+}
+export default Footer
